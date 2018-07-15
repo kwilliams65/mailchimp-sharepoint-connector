458a023d23d0ebda9acbc62ac3136706efe3bdda
Kate Williams <kate.j.williams65@gmail.com>
2018-07-15 14:21:34 -0400

    Add test for Mailchimp webhook verification route

--- test/routes.spec.ts
+++ test/routes.spec.ts
@@ -4,11 +4,16 @@ import { expect } from 'chai';
 describe('routes', () => {
 
     it('should contain correct number of routes', () => {
-        expect(routes).to.have.lengthOf(1);
+        expect(routes).to.have.lengthOf(2);
     });
 
     it('should contain mailchimp-webhook route', () => {
         const route = routes.find(x => x.route === '/mailchimp-webhook' && x.method === 'post');
+        expect(route).to.not.be.undefined;
+    });
+
+    it('should contain mailchimp-webhook verification route', () => {
+        const route = routes.find(x => x.route === '/mailchimp-webhook' && x.method === 'get');
         expect(route).to.not.be.undefined;
     });
 })
